2fdfdc0145da9f3f3b25dbbc6d0a33944f0f122b
Emirhan547 <emirhanhacioglu0@gmail.com>
2025-08-18 15:04:23 +0300

    Add

--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,8 @@ namespace DataAccessLayer.Abstract
         void Update(T t);
         void Delete(T t);
         List<T> GetList();
+        T GetById(int id);
+        List<T> GetListByFilter(Expression<Func<T,bool>>filter);
 
     }
 }
---
+++ TraversalCoreProject/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _PopularDestinations:ViewComponent
+    {
+        DestinationManager _destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _destinationManager.TGetList();
+            return View(values);
+        }
+
+    }
+}
